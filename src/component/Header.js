3ca20faa27d7c58c26fafafd1045bbf68df795a1
mitchelle <chigoziemitchellenkata@gmail.com>
2024-02-02 17:56:35 +0100

    no 2

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,17 +1,1 @@
 import React from "react";
-
-function Header(){
-    return(
-        
-        <section id="header">
-            <nav>
-            <ul>
-            <li><a href="#header">Home</a></li>
-            <li><a href="#projects">Projects</a></li>
-            </ul>
-        </nav>
-        </section>
-        
-    );
-}
-export default Header;
